6f53f11e4d9992ccfc6533c93a4787a31ef791b2
Pratyaksha37 <pratyakshashastri123@gmail.com>
2025-04-11 12:26:39 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/analyzer" element={<AnalyzerPage />} />
+      <Route path="analyzer" element={<AnalyzerPage />} /> 
     </Routes>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // Use HashRouter instead of BrowserRouter
 import App from "./App.jsx";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter> 
       <App />
     </HashRouter>
   </React.StrictMode>,
